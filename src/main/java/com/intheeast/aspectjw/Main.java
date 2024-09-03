fc307569a92e2a9660f8090a43c1ee0914e5c66b
intheeast <intheeast0725@gmail.com>
2024-09-03 19:08:45 +0900

    update comment

--- src/main/java/com/intheeast/aspectjw/Main.java
+++ src/main/java/com/intheeast/aspectjw/Main.java
@@ -10,7 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-	
+
 	public static void main(String ...args) {
 //		AnnotationConfigApplicationContext context =
 //				new AnnotationConfigApplicationContext(AppConfig.class);
--- src/main/java/com/intheeast/aspectjw/service/MyService.java
+++ src/main/java/com/intheeast/aspectjw/service/MyService.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyService {
 	
-	// ProfilingAspect LTE가 적용되지 않는 이유는?
-	// :Account.doSomething에서 performService를 호출하기 때문에
+	// ProfilingAspect LTW가 적용되지 않는 이유는?
+	// :Account.doSomething에서 performService를 호출하기 때문입니다.
 	public void performService() {
 		System.out.println("MyService::performService:Service is performing an operation");
 	}
