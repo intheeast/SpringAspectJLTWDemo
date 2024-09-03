957b12fc0ce8711c62d9e5d2101136cca9b6a002
intheeast <intheeast0725@gmail.com>
2024-09-03 19:03:49 +0900

    last commit

--- src/main/java/com/intheeast/aspectjw/Main.java
+++ src/main/java/com/intheeast/aspectjw/Main.java
@@ -10,8 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-	// 이 섹션의 소개에서 Spring을 사용하여 클래스 로더(ClassLoader)별로 선택적으로 LTW(Load-Time Weaving)를 활성화할 수 있다고 언급했는데, 이는 사실입니다.
-	// // 하지만, 이 예제에서는 LTW를 활성화하기 위해 Spring에서 제공하는 Java 에이전트를 사용합니다.
+	
 	public static void main(String ...args) {
 //		AnnotationConfigApplicationContext context =
 //				new AnnotationConfigApplicationContext(AppConfig.class);
